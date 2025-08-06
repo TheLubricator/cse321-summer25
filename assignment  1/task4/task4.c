77116fb4b23fb71a1818619ab096f31748bfd926
Faiyaz Zaman <51377374+TheLubricator@users.noreply.github.com>
2025-08-06 23:04:06 +0600

    initial completion

--- assignment  1/task4/task4.c
+++ assignment  1/task4/task4.c
@@ -10,15 +10,18 @@ int main (int argc,char *args[]){
     pid_t pid;
     int status;
     int array[]={2,4,1,3,8,7,9,6,5};
+    //we will manually enter these valeus in the execl command as this requires string conversion we haven't been taught
     pid=fork();
     if (pid>0){
         wait(&status);
+        printf("From parent process odd and even numbers of given array\n");
+        execl("./oddeven.exe", "./oddeven.exe", "2", "4", "1", "3", "8", "7", "9", "6", "5", NULL);
         return 0;
     }
     else if (pid==0){
-        char *args[size+2];
+        printf("From child process sorted array in descending order: \n");
+        execl("./sort.exe", "./sort.exe", "2", "4", "1", "3", "8", "7", "9", "6", "5", NULL);
 
-        execv("./","sort.exe",)
     }
-
+    return 0;
 }

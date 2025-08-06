00f85183c61ed2eb3b596e2af4164cd9e241df8e
Faiyaz Zaman <51377374+TheLubricator@users.noreply.github.com>
2025-08-06 22:32:02 +0600

    Add files via upload

---
+++ task1/task1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+#include<fcntl.h>
+#include <unistd.h>
+
+int main (int argc,char *args[]){
+    int fd;
+    FILE *fp;
+
+    fd=open(args[1],O_CREAT|O_WRONLY);
+
+
+    while(true){
+        char input[50];
+        printf("Please Enter a string: ");
+        scanf("%s",input);
+        if (input[strlen(input)-2]=='-' && input[strlen(input)-1]=='1') {
+            close(fd);
+            break;
+        }
+        write(fd,input,strlen(input));
+        write(fd,"\n",1);
+        lseek(fd,0,SEEK_END);
+
+    }
+    close(fd);
+    return 0;
+}

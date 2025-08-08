b5d7857415324553b93fe10d663d90ff2ba18aec
Faiyaz Zaman <51377374+TheLubricator@users.noreply.github.com>
2025-08-09 01:14:13 +0600

    .

---
+++ assignment 2/task2/task2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<pthread.h>
+#include<semaphore.h>
+
+int *served_counter=0;
+int studen_ids[10]={0,1,2,3,4,5,6,7,8,9};
+pthread_mutex_t mutex_waiting,mutex_serving;
+sem_t student_wait;
+int chair_queue[3];
+int main(){
+
+}

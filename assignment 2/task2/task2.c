cd48a5d631e2878fefcf6095ae2df4b69d2b55ca
Faiyaz Zaman <51377374+TheLubricator@users.noreply.github.com>
2025-08-09 23:33:19 +0600

    .

--- assignment 2/task2/task2.c
+++ assignment 2/task2/task2.c
@@ -5,10 +5,13 @@
 #include<semaphore.h>
 
 int *served_counter=0;
+int *total_students_attempted=0;
 int studen_ids[10]={0,1,2,3,4,5,6,7,8,9};
-pthread_mutex_t mutex_waiting,mutex_serving;
+pthread_mutex_t mutex_waiting;
 sem_t student_wait;
-int chair_queue[3];
+int chair_queue[3]={-1,-1,-1};
+
 int main(){
+
 
 }
